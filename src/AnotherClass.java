63ce44cf068be831e03693f052077a569db055b2
Shhevla <nathanlink33@gmail.com>
2023-12-18 15:17:51 +0100

    Feat: nouvelle modif

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -1,6 +1,6 @@
 public class AnotherClass {
     public static void main(String[] args) {
         System.out.println("Create Conflict");
-        System.out.println("New modif");
+        System.out.println("New modif amelipo");
     }
 }
